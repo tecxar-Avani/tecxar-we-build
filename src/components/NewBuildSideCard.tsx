24cb8b3306f5b843eb1fd40b0a434865b873b546
AvaniChauhan28 <avani.chauhan@tecxar.io>
2022-11-17 14:36:53 +0530

    worked on buttons of new build page

--- src/components/NewBuildSideCard.tsx
+++ src/components/NewBuildSideCard.tsx
@@ -1,10 +1,22 @@
 import Link from "next/link";
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Image } from "react-bootstrap";
+import Button from 'react-bootstrap/Button';
+import Overlay from 'react-bootstrap/Overlay';
 import CustomButton from "./Button";
 import VideoCard from "./VideoCard";
-import { Button, Input, Modal } from "antd";
+import { MenuProps, Tooltip } from "antd";
+import { Dropdown, Space, Typography } from "antd";
+import {  Input, Modal } from "antd";
 const NewBuildSideCard = (props: any) => {
+  
+    const [polarisation, setPolarisation] = useState(false);
+    const  polarisations = useRef(null);
+    const [difficulty,setDifficulty] = useState(false);
+    const target2 = useRef(null);
+    const [type , setType] = useState(false);
+    const typeVideo = useRef(null);
+  
   const { TextArea } = Input;
   const videosData = [
     {
@@ -13,35 +25,36 @@ const NewBuildSideCard = (props: any) => {
       videoUrl: "img/RectangleVideoImg.png",
     },
   ];
-  const warning = () => {
-    Modal.warning({
-      title: '',
-      content: 'Please make an account to save your build - dont let all your hard work go to waste!Login via Google',
-    });
-  };
+ 
+ 
   return (
     <div>
-          {videosData.length > 0 &&
-            videosData.map((videoData, index) => (
-              <div className="videoProfile" key={index}>
-                <Link href="/newBuild">
-                  <VideoCard VideoCardData={videoData} />
-                </Link>
-              </div>
-            ))}
-            <TextArea placeholder="What is Video About?" autoSize={{ minRows: 3, maxRows: 5 }} className="py-0 newBuild"/>
-          <hr className="border-dark" />
-
-          <div className="mx-4">
-            <CustomButton title="Group" className="btn1 px-4 py-3" />
-            <CustomButton title="New row" className="btn2  px-4 py-3 ms-2" />
+      {videosData.length > 0 &&
+        videosData.map((videoData, index) => (
+          <div className="videoProfile" key={index}>
+            <Link href="/newBuild">
+              <VideoCard VideoCardData={videoData} />
+            </Link>
           </div>
-          <hr className="border-dark"/>
-          <h2> Traingle</h2>
+        ))}
+      <TextArea
+        placeholder="What is Video About?"
+        autoSize={{ minRows: 3, maxRows: 5 }}
+        className="py-0 newBuild"
+      />
+      <hr className="border-dark" />
 
-          <hr className="border-dark" />
+      <div className="mx-4">
+        <CustomButton title="Group" className="btn1 px-4 py-3" />
+        <CustomButton title="New row" className="btn2  px-4 py-3 ms-2" />
+      </div>
+      <hr className="border-dark" />
+      <h2> Traingle</h2>
 
-          <div className="d-flex ml-2 ms-1 ">
+      <hr className="border-dark" />
+
+      <div className="d-flex ml-2 ms-1 ">
+      <Button variant="" ref={typeVideo} onClick={() => setType(!type)}>
             <figure className="ms-3">
               <Image
                 src="../img/typeof.svg "
@@ -53,7 +66,26 @@ const NewBuildSideCard = (props: any) => {
                 Type of <br></br>Video
               </figcaption>
             </figure>
-
+            </Button>
+            <Overlay target={typeVideo.current} show={type} placement="right">
+        {({ placement, arrowProps, show: _show, popper, ...props }) => (
+          <div
+            {...props}
+            style={{
+              position: 'absolute',
+              backgroundColor: 'black',
+              color: 'white',
+              borderRadius: 3,
+              ...props.style,
+            }}
+          >
+           <button className="high-btn">Practical</button><br></br>
+           <button className="high-btn">Theoretical</button><br></br>
+           
+          </div>
+        )}
+      </Overlay>
+            <Button variant="" ref={polarisations} onClick={() => setPolarisation(!polarisation)}>
             <figure className="ms-3">
               <Image
                 src="../img/polarisation.svg"
@@ -65,7 +97,27 @@ const NewBuildSideCard = (props: any) => {
                 Polarisation <br></br>potental
               </figcaption>
             </figure>
-
+            </Button>
+            <Overlay target={polarisations.current} show={polarisation} placement="right">
+        {({ placement, arrowProps, show: _show, popper, ...props }) => (
+          <div
+            {...props}
+            style={{
+              position: 'absolute',
+              backgroundColor: 'black',
+              color: 'white',
+              borderRadius: 3,
+              ...props.style,
+            }}
+          >
+           <button className="high-btn">Low</button>
+           <button className="high-btn">Medium</button><br></br>
+           <button className="high-btn">High</button>
+           <button className="high-btn mt-0">Very <br></br>High</button>
+          </div>
+        )}
+      </Overlay>
+      <Button variant="" ref={target2} onClick={() => setDifficulty(!difficulty)}>
             <figure className="ms-3">
               <Image
                 src="../img/difficulty.svg"
@@ -76,25 +128,49 @@ const NewBuildSideCard = (props: any) => {
 
               <figcaption className="text-center mt-2">Difficulty</figcaption>
             </figure>
+            </Button>
+            <Overlay target={target2.current} show={difficulty} placement="left">
+        {({ placement, arrowProps, show: _show, popper, ...props }) => (
+          <div
+            {...props}
+            style={{
+              position: 'absolute',
+              backgroundColor: 'black',
+              color: 'white',
+              borderRadius: 3,
+              ...props.style,
+            }}
+          >
+           <button className="high-btn">Low</button>
+           <button className="high-btn">Medium</button><br></br>
+           <button className="high-btn">High</button>
+           <button className="high-btn mt-0">Very <br></br>High</button>
           </div>
+        )}
+      </Overlay>
+      </div>
 
-          <hr className="border-dark mb-3  " />
-          <div className="d-flex owd bd-highlight">
-            <div className="save bd-highlight  ">
-              <Image src="../img/save.svg" className="ms-2" onClick={warning}/>
-            </div>
-            <div className="backward bd-highlight">
-              <Image src="../img/backward.svg" className="ms-5 me-1" />
-            </div>
-            <div className=" forward bd-highlight">
-              <Image src="../img/forward.svg" className="me-5" />
-            </div>
-            <div className=" delt bd-highlight">
-              <Image src="../img/delt.svg" className="me-3" />
-            </div>
-          </div>
+      <hr className="border-dark mb-3  " />
+      <div className="d-flex owd bd-highlight">
+      <Tooltip placement="topLeft" title="Please make an account to save your build - dont let all your hard work go to waste!Login via Google" arrowPointAtCenter color="#FAEFAF">
+      <div className="save bd-highlight  ">
+          <Image src="../img/save.svg" className="ms-2" />
         </div>
-       
+    </Tooltip>
+        {/* <div className="save bd-highlight  ">
+          <Image src="../img/save.svg" className="ms-2" onClick={warning} />
+        </div> */}
+        <div className="backward bd-highlight">
+          <Image src="../img/backward.svg" className="ms-5 me-1" />
+        </div>
+        <div className=" forward bd-highlight">
+          <Image src="../img/forward.svg" className="me-5" />
+        </div>
+        <div className=" delt bd-highlight">
+          <Image src="../img/delt.svg" className="me-3" />
+        </div>
+      </div>
+    </div>
   );
 };
 
