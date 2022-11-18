b91818a138f10df1e490d940f429bb7582240834
AvaniChauhan28 <avani.chauhan@tecxar.io>
2022-11-18 15:25:00 +0530

    work on video card

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -7,25 +7,27 @@ import { Card, CardImg, Col, Row } from "react-bootstrap";
 // }
 
 const VideoCard = (props: any) => {
+  console.log("$$$$$$$$$$$$",props.VideoCardData.videoUrl)
   return (
-   
-    <Card className="p-0 border-0 rounded-0 mx-2">
-      {props.VideoCardData.title && (
-        <Card.Header>{props.VideoCardData.title}</Card.Header>
-      )}
-      <Card.Body className="p-0 rounded-0">
+
+    // <Card className="p-0 border-0 rounded-0 mx-2">
+    //   {props.VideoCardData.title && (
+    //     <Card.Header>{props.VideoCardData.title}</Card.Header>
+    //   )}
+    //   <Card.Body className="p-0 rounded-0">
         <div className="position-relative">
-          <CardImg
+          {/* <iframe
             className="rounded-0"
-            src={`/${props.VideoCardData.videoUrl}`}
-          ></CardImg>
+            src={`` }
+          ></iframe> */}
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/eJ1UDdQbE-k" title="YouTube video player" allow=" picture-in-picture" allowFullScreen={true}></iframe>
           <div className="position-absolute bottom-0 end-0 px-2 mb-2 py-1 bg-black rounded">
             <span className="text-white">12:45</span>
           </div>
         </div>
-        <Card.Text className="">{props.VideoCardData.subTitle}</Card.Text>
-      </Card.Body>
-      </Card>
+      //   <Card.Text className="">{props.VideoCardData.subTitle}</Card.Text>
+      // </Card.Body>
+      // </Card>
   );
 };
 
