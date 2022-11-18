a4ac179f77e8dd392413053d7c03b124be0786f0
Tecxar Dev <dev@tecxar.io>
2022-11-18 12:18:02 +0530

    removed unused code

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,21 +1,12 @@
 import { Input, Form } from "antd";
 import Link from "next/link";
-import { useAppDispatch, useAppSelector } from "../hooks";
-import React, { useState, useEffect } from "react";
-import { Card, CardImg, Col, Row } from "react-bootstrap";
-import { buildSelector, getBuildByUrl } from "../store/reducers/build.reducer";
+import React from "react";
+
 const { Search } = Input;
 const onSearch = (value: any) => console.log(value);
 
 const SearchBar = (props:any) => {
-  const [url, setUrl] = useState("");
-  const dispatch = useAppDispatch();
-  const build = useAppSelector(buildSelector);
-  useEffect(() => {
-    if(url !== ""){
-    dispatch(getBuildByUrl(url));
-    }
-  }, [url]);
+ 
   return (
     <>
       <div className="searchCard mt-4 justify-content-center">
