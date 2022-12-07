b92c9e9a8b9831db718b73ecb971d6b081f4782f
AvaniChauhan28 <avani.chauhan@tecxar.io>
2022-12-07 15:08:57 +0530

    google login issue solved

--- server/configs/passport.ts
+++ server/configs/passport.ts
@@ -6,7 +6,7 @@ export const googleStrategy = new GoogleStrategy(
   {
     clientID: config.google.clientId,
     clientSecret: config.google.clientSecret,
-    callbackURL: config.google.callbackUrl,
+    callbackURL: 'https://webuild.tecxar.io/api/google_callback',
   },
   function (accessToken, refreshToken, profile, cb) {
     return cb(null, profile);
